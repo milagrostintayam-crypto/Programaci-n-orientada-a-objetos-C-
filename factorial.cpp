c65363a7a38b2cabffd78e901f02a5a690588242
DIANA <milagrostintayam@gmail.com>
2026-02-27 22:14:22 -0500

    Factorial

---
+++ factorial.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int datUser;
+    int Result = 1;
+    while (true) {
+        cout<<"Bienvenido"<<endl;
+    cout<<"Ingrese su numero: "<<endl;
+    cin>>datUser;
+
+    for 
+    (int i = datUser; i > 0; i--) {
+        Result *= i;
+        if (i==1) {
+        cout << "El factorial es: " << Result << endl;
+        }
+    }
+    }
+    return 0;
+}   
+    
+    
+
+
+
+
+
+
+    //while (datUser > 0)
+    //{
+    //   if (datUser = 1) {
+      //  Result 
+      // }
+      // datUser - 1; datUser --;
+    //Result = datUser * datUser - 1;
